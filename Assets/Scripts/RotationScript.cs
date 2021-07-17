c4424e8a5f966514c65e18ee2b1faecf5c64426f
Ivan <ivan.kashytski.mkx@gmail.com>
2021-07-17 23:02:08 +0300

    Add_lockRotation

---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,14 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.autorotateToPortrait = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+        Screen.orientation = ScreenOrientation.AutoRotation;
+    }
+}
