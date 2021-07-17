9134088f3090338b9391a7fe7436c4b679abd1f6
Ivan <ivan.kashytski.mkx@gmail.com>
2021-07-17 21:14:13 +0300

    add_finishCondition

--- Assets/Scripts/FinishCounter.cs
+++ Assets/Scripts/FinishCounter.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FinishCounter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Text counter;
+    [SerializeField] GameObject stone;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        int counterInt = int.Parse(counter.text);
+        if (counterInt > 220)
+            stone.SetActive(false);
     }
 }
--- Assets/Scripts/SceneScript.cs
+++ Assets/Scripts/SceneScript.cs
@@ -6,11 +6,21 @@ using UnityEngine.SceneManagement;
 public class SceneScript : MonoBehaviour
 {
     [SerializeField] GameObject Player;
+    [SerializeField] GameObject gameStart;
+    [SerializeField] GameObject[] level_thing;
+ 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
             if (Player == null)
                 SceneManager.LoadScene("Scene_1");
             else Application.Quit();
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            gameStart.SetActive(false);
+            foreach (var i in level_thing)
+                i.SetActive(true);
+        }
     }
 }
